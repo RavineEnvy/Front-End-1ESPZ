9a5aeecb55305d76797638d32a552884a9ba3978
Guilherme Freire Pires <guiga_freire06@hotmail.com>
2026-03-25 20:59:55 -0300

    Meu perfil

---
+++ app.js
@@ -0,0 +1,22 @@
+//topo - Header com título e navegação
+//Blocos de conteúdo - Selections para serapar assuntos
+//Conteúdo Principal - Main com a informação central
+//Rodapé - informação do final do site
+
+//TAGS ESTRUTUARIS
+
+//<main> - Conteúdo da página. É único e contém a informação do documento.
+
+//<section> - Separar blocos ou assuntos dentro da página. Pode ter múltiplas seções dentro da main.
+
+//<footer> Todapé da página. Geralmente contém informação de contato, direitos autorais ou links adicionais
+
+//<nav> Define uma área de navegação com links, essencial para a usabilidade e para guiar o usuário pela página.
+
+//<aside> Contém conteúdo complementar ou secundário, como barras laterais, anúncios ou informações relacionadas ao conteúdo principal.
+
+//Tags de Listas
+
+//<ul> e <li> - Lista não ordenada com marcadores
+//<ol> e <li> - Lista ordena com números
+//Use "ul" quando a ordem não importa (hobbies, caracteristicas). Use "ol" quando há sequência (passos de receita, intruções).
